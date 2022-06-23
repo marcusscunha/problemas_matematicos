76be8facaabd279687496a0564f22cec31a20231
Marcus Vitor da Cunha Nascimento <106033361+marcusscunha@users.noreply.github.com>
2022-06-23 13:10:33 -0300

    Add files via upload

---
+++ problemas_matematicos.java
@@ -0,0 +1,27 @@
+package aplicacao;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class problemas_matematicos {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		double largura = sc.nextDouble();
+		double comprimento = sc.nextDouble();
+ 		double metroQuadrado = sc.nextDouble();
+	
+		double area = largura*comprimento;
+		double preco = area*metroQuadrado;
+		
+		System.out.printf("ÁREA =%.2f ",area);
+		System.out.printf("%nPREÇO =%.2f ",preco);
+		
+		
+sc.close();
+	}
+
+}
