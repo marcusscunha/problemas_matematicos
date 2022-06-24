65985e0d3a4957c0ab1714e713c76e7ccc560901
Marcus Vitor da Cunha Nascimento <106033361+marcusscunha@users.noreply.github.com>
2022-06-24 18:43:36 -0300

    Add files via upload

---
+++ raio_do_circulo.java
@@ -0,0 +1,24 @@
+package aplicacao;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class raio_do_circulo {
+
+	public static void main(String[] args) {
+		
+	Locale.setDefault(Locale.US);
+	Scanner sc=new Scanner (System.in);
+	
+	double pi = 3.14159;
+	
+	double x = sc.nextDouble();
+	
+	double z = x*x*pi;
+	
+	System.out.printf("o valor do raio foi:%.4f",z);
+	//colocar 4 casas apó a virgula 
+	sc.close();
+	}
+
+}
